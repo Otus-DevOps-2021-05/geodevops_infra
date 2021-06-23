af7d7bb9021f0e079acbce436d5d8f42cb5a168d
oleg <geodevops85@yandex.ru>
2021-06-23 11:38:05 +0400

    change mongo version

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 sleep 5s
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sleep 10s
 sudo apt-get update
 sleep 5s
 sudo apt-get install -y mongodb-org
-sleep 5s
+sleep 10s
 sudo systemctl start mongod
 sleep 5s
 sudo systemctl enable mongod
